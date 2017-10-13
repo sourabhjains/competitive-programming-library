2f91e7674a3c2f2de8061c9545cdae3cba6d854f
Dhaval Shah <dhavals2912@gmail.com>
2017-10-13 23:15:56 +0530

    Added floddfill generic algorihm to fill colors with variable offset in defined direction

---
+++ template.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#define fori(i,n) for(int i=0;i<n;i++)
+#define fors(i,init,n,step) for(int i=init;i<n;i+=step)
+
+using namespace std;
+
+typedef int nd;
+
+int main(){
+	nd c;
+	cin>>c;
+	
+	return 0;
+}
